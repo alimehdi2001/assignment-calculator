9a25a51b430f666bbbc1f7ea29fbe1fec28c1eca
Ali mehdi <110098560+alimehdi2001@users.noreply.github.com>
2024-03-05 10:02:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+// ---------------- Assignment # 3 calculator---------------
+// Build a calculator and performed the arithmetic operation
+// (+,-,*,/,%)
+
+let num1 = +prompt("Enter the first Number");
+let num2 = +prompt("Enter the second Number");
+let sum = num1+num2;
+document.write("sum of two numbers is = " , num1+num2 );
+document.write("<br>");
+
+let subtract = num1-num2;
+document.write( " subtraction of two numbers is = ", num1-num2);
+document.write("<br>");
+
+let mul = num1*num2;
+document.write( " Multiplication of two numbers is = ", num1*num2);
+document.write("<br>");
+
+let divide = num1/num2;
+document.write( " division of two numbers is = ", num1/num2);
+
+
+
